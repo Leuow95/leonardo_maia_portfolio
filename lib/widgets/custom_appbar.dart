60dec1266c44b424ba0b3c3e2b0c5dea93554928
leonardomaia <leuow95@gmail.com>
2024-05-03 16:50:20 -0400

    feat: added link navigation

--- lib/widgets/custom_appbar.dart
+++ lib/widgets/custom_appbar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CustomAppBar({super.key});
@@ -7,9 +9,17 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       title: const Text("Leonardo Maia"),
-      actions: const [
-        Text("Github"),
-        Text("Linkedin"),
+      actions: [
+        InkWell(
+          onTap: () => launchUrl(Uri.parse("https://github.com/Leuow95")),
+          child: SvgPicture.asset("assets/icons/github_icon.svg", width: 32),
+        ),
+        const SizedBox(width: 16),
+        InkWell(
+          onTap: () => launchUrl(Uri.parse("https://www.linkedin.com/in/leonardo-maia-flutter/")),
+          child: SvgPicture.asset("assets/icons/linkedin_icon.svg", width: 32),
+        ),
+        const SizedBox(width: 32),
       ],
     );
   }
