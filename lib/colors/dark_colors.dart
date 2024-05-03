24bc44eec4413245d635511d74e32750bf7b2a84
leonardomaia <leuow95@gmail.com>
2024-05-03 16:57:41 -0400

    feat: improved home

---
+++ lib/colors/dark_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LMColors extends ColorScheme {
+  const LMColors({
+    required super.brightness,
+    required super.primary,
+    required super.onPrimary,
+    required super.secondary,
+    required super.onSecondary,
+    required super.error,
+    required super.onError,
+    required super.background,
+    required super.onBackground,
+    required super.surface,
+    required super.onSurface,
+  });
+}
